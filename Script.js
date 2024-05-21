6ebce3cfcb1d536b4b292a4f16c9bbcac080066a
gabriel-leal <gabrielealmenezes@gmail.com>
2024-05-21 19:28:27 -0300

    ,

--- Script.js
+++ Script.js
@@ -1,11 +1,3 @@
-function clickMenu() {
-    if (itens.style.display == 'block') {
-      itens.style.display = 'none'
-    }  else {
-      itens.style.display = 'block'
-    }
-  }
-
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
